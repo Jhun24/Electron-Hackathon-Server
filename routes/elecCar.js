3be2ba6559b9ac545120a0ed20e718abd6a0eaa0
Jhun24 <jjanghuni102@naver.com>
2017-11-04 20:30:26 +0900

    [location] bug fix

--- routes/elecCar.js
+++ routes/elecCar.js
@@ -7,7 +7,7 @@ function elecCar(app) {
 
     app.get('/location/chargingStation',(req,res)=>{
         "use strict";
-        request({"url":"http://open.ev.or.kr:8080/openapi/services/rest/EvChargerService?serviceKey=QP2h4bHuyL7jObG%2B92OQa%2F1vZLmO1wr9VqMkdwTP3IW80Ie%2F1Y7KTa0fbzRsQNYe3kdDtECUljR%2FWNuPDJTftQ%3D%3D"},(err,response,body)=>{
+        request.get({"url":"http://open.ev.or.kr:8080/openapi/services/rest/EvChargerService?serviceKey=QP2h4bHuyL7jObG%2B92OQa%2F1vZLmO1wr9VqMkdwTP3IW80Ie%2F1Y7KTa0fbzRsQNYe3kdDtECUljR%2FWNuPDJTftQ%3D%3D"},(err,response,body)=>{
             if(err) throw err;
             res.send(body);
         });
